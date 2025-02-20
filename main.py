0c2c4c85f9684ff3a9bd4bb41a306be497be18e5
Lulu-gaby <lulugabyxxx@gmail.com>
2025-02-20 22:17:45 +0300

    add new names and change access

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 class User():
     def __init__(self, id, name):
         self.__id = id
-        self.__name = name
-        self.__level_access = "user"
+        self._name = name
+        self._level_access = "user"
 
     def get_id(self):
         return self.__id
 
     def get_name(self):
-        return self.__name
+        return self._name
 
     def get_level_access(self):
-        return self.__level_access
+        return self._level_access
 
     def set_name(self, name):
-        self.__name = name
+        self._name = name
 
     def __str__(self):
-        return f"ID: {self.__id}, Имя: {self.__name}, Уровень доступа: {self.__level_access}"
+        return f"ID: {self.__id}, Имя: {self._name}, Уровень доступа: {self._level_access}"
 
 
 class Admin(User):
@@ -35,11 +35,11 @@ class Admin(User):
 
 users_list = []
 
-user1 = User ("n001", "Василий")
-user2 = User ("n002", "Мария")
-user3 = User ("n003", "Николай")
+user1 = User ("1-1", "Петр")
+user2 = User ("1-2", "Светлана")
+user3 = User ("1-3", "Ольга")
 
-admin = Admin ("a001", "Александр")
+admin = Admin ("A-1", "Виктор")
 
 print("Имя пользователя:", user2.get_name())
 admin.add_user(users_list, user1)
